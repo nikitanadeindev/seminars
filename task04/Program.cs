27154c8d79ac319eb325932c43bf3a347db85115
c1aymore <nikitanadeindev@gmail.com>
2022-11-14 12:25:09 +0300

    Fix task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -7,19 +7,9 @@ Console.WriteLine("Введите второе целое число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье целое число");
 int number3 = Convert.ToInt32(Console.ReadLine());
-int max = 0;
+int max = number1;
 
-if (number1 > number2 && number1 > number3)
-{
-    max = number1;
-}
-else if(number2 > number1 && number2 > number3)
-{
-    max = number2;
-}
-else
-{
-    max = number3;
-}
+if (max < number2) max = number2;
+if (max < number3) max = number3;
 
 Console.WriteLine($"Наибольшее число = {max}");
