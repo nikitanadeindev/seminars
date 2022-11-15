e6bc9062675032c7ac35c698a07ae14ab6bf793b
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:37:52 +0300

    Solved task13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100)
+    Console.WriteLine($"{number} -> третьей цифры нет");
+else
+{
+    int betweenNumber = number;
+    while (betweenNumber > 999)
+            betweenNumber = betweenNumber / 10;
+    Console.WriteLine($"{number} -> {betweenNumber % 10}");
+}
+
