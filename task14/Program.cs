d6beec276874666f3f4e73a4afcbd1fe127abb43
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:20:28 +0300

    Solved task14

---
+++ task14/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine($"{number} -> да");
+else
+    Console.WriteLine($"{number} -> нет");
