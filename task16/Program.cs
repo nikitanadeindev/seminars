4d907f177dd52ed4d0ae017af4c11efa1ae6fde5
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:28:58 +0300

    Solved task16

---
+++ task16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int squareFirst = firstNumber * firstNumber;
+int squareSecond = firstNumber / secondNumber;
+
+if(secondNumber == squareFirst || squareSecond == secondNumber)
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> да");
+else
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> нет");
