fb7edbc700430927a5a7b02893e86a7e17de8a73
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:09:40 +0300

    Solved task09

---
+++ task09/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10, 99);
+
+int div = number / 10;
+int mod = number % 10;
+int max;
+
+if(div > mod)
+{
+    max = div;
+    Console.WriteLine($"{number} -> {max}");
+}
+else
+{
+    max = mod;
+    Console.WriteLine($"{number} -> {max}");
+}
