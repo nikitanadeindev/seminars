6b47420f5f2f0e1909dbccc75ede16f50e9ae14b
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:42:52 +0300

    Solved task15

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите цифру обозначающую день недели: ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if(dayOfWeek < 1 || dayOfWeek > 7)
+    Console.WriteLine($"{dayOfWeek} -> такого дня недели нет");
+else if(dayOfWeek == 6 || dayOfWeek == 7)
+    Console.WriteLine($"{dayOfWeek} -> да");
+else
+    Console.WriteLine($"{dayOfWeek} -> нет");
