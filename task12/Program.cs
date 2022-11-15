f093567b3eb4d77cbcd07fa3ff3d7c4f5e548229
c1aymore <nikitanadeindev@gmail.com>
2022-11-15 22:18:32 +0300

    Solved task12

---
+++ task12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int mod = firstNumber % secondNumber;
+
+if(mod == 0)
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> кратно");
+else
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> не кратно, остаток {mod}");
