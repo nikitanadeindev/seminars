f21325e67b149afd0b42dc1017c9053eb67d09ad
c1aymore <nikitanadeindev@gmail.com>
2022-11-19 03:37:28 +0300

    Solved task18

---
+++ task18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение четверти: ");
+string quarter  = Console.ReadLine();
+
+string Range(string x)
+{
+    if(x == "1") return "x > 0 и y > 0";
+    if(x == "2") return "x < 0 и y > 0";
+    if(x == "3") return "x < 0 и y < 0";
+    if(x == "4") return "x > 0 и y < 0";
+    return "Введено некорректное значение четверти";
+}
+
+string range = Range(quarter);
+
+Console.WriteLine(range);
