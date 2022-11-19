589c312f95fca49c8e4227af37e122388d4480e7
c1aymore <nikitanadeindev@gmail.com>
2022-11-19 03:22:17 +0300

    Solved task17

---
+++ task17/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите координаты точки x != 0: ");
+int x  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки y != 0: ");
+int y  = Convert.ToInt32(Console.ReadLine());
+
+int Quarter(int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return 1;
+    if(xc < 0 && yc > 0) return 2;
+    if(xc < 0 && yc < 0) return 3;
+    if(xc > 0 && yc < 0) return 4;
+    return 0;
+}
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+        ? $"Указанные координаты соответствуют четверти -> {quarter}" 
+        : "Введены некорректные координаты";
+Console.WriteLine(result); 
