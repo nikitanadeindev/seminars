cc2728aeff2cd0fef4f7bc9e5d97cc701aebf65d
c1aymore <nikitanadeindev@gmail.com>
2022-11-19 04:18:59 +0300

    Solved task22

---
+++ task22/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void RunningSquare(int num)
+{
+    for(int i = 1; i <= number; ++i)
+    {
+        Console.Write((Math.Pow(i, 2) + " "));
+    }
+}
+
+if(number < 1)
+    Console.WriteLine("Введите положительное число > 0");
+else
+    RunningSquare(number);
