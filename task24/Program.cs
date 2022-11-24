0cf9c8a6a1dc56addd332f87db6a0e8c84c56e96
c1aymore <nikitanadeindev@gmail.com>
2022-11-24 07:37:15 +0000

    Solved task24

---
+++ task24/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for(int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumDigits}");
