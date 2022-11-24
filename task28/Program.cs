7c18430800f79917cd6aa5ba766b0b401f1bc7d7
c1aymore <nikitanadeindev@gmail.com>
2022-11-24 08:14:01 +0000

    Solved task28

---
+++ task28/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int IncreaseDigits(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+
+    return sum;
+}
+
+if(number > 0)
+{
+    int increaseDigits = IncreaseDigits(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} =  {increaseDigits}");
+}
+else
+{
+    Console.WriteLine("Требуется ввести натуральное число");
+}
