77176bfc2b77d152a798c12d3f36c52e701bd911
c1aymore <nikitanadeindev@gmail.com>
2022-11-26 10:07:22 +0000

    Solved task32

---
+++ task32/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.Length; i++)
+        arr[i] = rnd.Next(min, max + 1);
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) 
+            Console.Write(arr[i] + ", ");
+        else 
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+int[] ElemSwap(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        arr[i] *= -1;
+
+    return arr;
+}
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(arraySize, min, max);
+PrintArray(array);
+Console.Write(" -> ");
+int[] swappedArray = ElemSwap(array);
+PrintArray(swappedArray);
