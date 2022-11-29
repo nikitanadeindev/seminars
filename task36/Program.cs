7c6bef301cd771f983421fe9df6c8230f5f39ddf
c1aymore <nikitanadeindev@gmail.com>
2022-11-29 07:52:14 +0000

    Solved task36

---
+++ task36/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.Length; i++)
+        arr[i] = rnd.Next(min, max + 1);
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) 
+            Console.Write(arr[i] + ", ");
+        else 
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+int ElemSum(int[] arr)
+{
+    int sum = 0;
+    for(int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(arraySize, min, max);
+PrintArray(array);
+int result = ElemSum(array);
+Console.Write(" -> " + result);
