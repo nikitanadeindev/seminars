8bade61e1beb744de42de6ccdb754bc32b06a0a5
c1aymore <nikitanadeindev@gmail.com>
2022-11-30 09:15:11 +0000

    Solved task39

---
+++ task39/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.Length; i++)
+        arr[i] = rnd.Next(min, max + 1);
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) 
+            Console.Write(arr[i] + ", ");
+        else 
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateArray(7, 1, 9);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
