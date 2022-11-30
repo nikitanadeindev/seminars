d99f520491b045e340d30db518de5cba532836a6
c1aymore <nikitanadeindev@gmail.com>
2022-11-30 10:43:25 +0000

    Solved task45

---
+++ task45/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < arr.Length; i++)
+        arr[i] = rnd.Next(min, max + 1);
+
+    return arr;
+}
+
+int[] ArrayCopy(int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+
+    for(int i = 0; i < arr.Length; i++)
+        newArray[i] = arr[i];
+    
+    return newArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + " ");
+        else
+            Console.Write(arr[i]);
+    }
+}
+
+int[] array = CreateArray(6, 1, 9);
+int[] someArray = ArrayCopy(array);
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(someArray);
