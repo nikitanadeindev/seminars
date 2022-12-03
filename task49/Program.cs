ee0d0363d04be7d6a3fccf0dbdfea869de6e5191
c1aymore <nikitanadeindev@gmail.com>
2022-12-03 11:05:48 +0000

    Solved task49

---
+++ task49/Program.cs
@@ -0,0 +1,47 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SquareEvenMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(j % 2 == 0 && i % 2 == 0)
+                matrix[i, j] *= matrix[i,j];
+        }
+    }
+}
+
+int[,] array2D = CreateMatrix(3, 4, -50, 50);
+Console.WriteLine("Initial matrix");
+PrintMatrix(array2D);
+Console.WriteLine("Square matrix");
+SquareEvenMatrix(array2D);
+PrintMatrix(array2D);
