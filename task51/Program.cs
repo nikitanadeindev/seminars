acb0a56dd99ee24ee4b73af8b9969f14434998cb
c1aymore <nikitanadeindev@gmail.com>
2022-12-03 11:06:09 +0000

    Solved task 51

---
+++ task51/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int DiagonalSum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) sum += matrix[i, j];
+        }
+    }
+    return sum;
+}
+
+int[,] array2D = CreateMatrix(3, 4, 1, 9);
+int result = DiagonalSum(array2D);
+Console.WriteLine("Сумма элементов главной диагонали: " + result);
+PrintMatrix(array2D);
