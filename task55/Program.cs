21054a3ae8b43f10bd6024ba764ba0f93955df5d
c1aymore <nikitanadeindev@gmail.com>
2022-12-08 12:24:08 +0000

    Solved task55

---
+++ task55/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SwapRowsAndColumns(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+int[,] matr = CreateMatrix(4, 4, 1, 9);
+bool notEquals = (matr.GetLength(0) != matr.GetLength(1));
+if (notEquals)
+    Console.WriteLine("Поменять строки и столбцы невозможно");
+else
+{
+    Console.WriteLine("Изначальная матрица");
+    PrintMatrix(matr);
+    Console.WriteLine("Измененная матрица");
+    SwapRowsAndColumns(matr);
+    PrintMatrix(matr);
+}
