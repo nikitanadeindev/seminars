ea0a580bdc19bd482901e08f1a118ff9630ddb36
nikita <nikitanadeindev@gmail.com>
2022-12-13 15:57:14 +0000

    Solved task67

---
+++ task67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 67. Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в числе = " + DigitsSum(Math.Abs(number)));
+
+int DigitsSum(int n)
+{
+    return (n != 0) ? n % 10 + DigitsSum(n / 10) : 0;
+}
+
