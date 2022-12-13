07b073442a71f9cd3f2e59ebd3dc215bb5fa4f74
nikita <nikitanadeindev@gmail.com>
2022-12-13 14:39:53 +0000

    Solved task63

---
+++ task63/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int n)
+{
+    if(n < 1) return;
+    NaturalNumbers(n - 1);
+    Console.Write(n + " ");
+}
+
+NaturalNumbers(number);
